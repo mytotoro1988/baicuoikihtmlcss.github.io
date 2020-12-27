38e452895d5dbad4bd487af347d99a7f652212cc
mytotoro1988 <lehuucuong.vnua@gmail.com>
2020-12-28 02:40:27 +0700

    28

--- owl.js
+++ owl.js
@@ -36,3 +36,22 @@
         }
     })
   });
+
+  $(document).ready(function(){
+    $('.owl-carousel3').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:4
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
+  });
